6d0e821c995c5883ec2f4f43b2787f0c0a4e376d
81245995@qq.com <81245995@qq.com>
2019-06-21 17:37:48 +0800

    移除无用pkg

--- src/index.js
+++ src/index.js
@@ -4,38 +4,8 @@ import {
 } from 'utility-mar';
 import 'styles';
 import Vue from 'vue';
-import {
-  Provider,
-} from 'react-redux';
-import {
-  createStore,
-} from 'redux';
-import {
-  Route,
-  Router,
-} from 'react-router-dom';
-import history from 'router/history';
-import ReactDOM from 'react-dom';
-import reducers from './reducers';
-// 主应用app入口文件
 
 import App from "./App.vue";
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <Router history={history}>
-//       <Route path="/" component={App} />
-//     </Router>
-//   </Provider>,
-//   document.getElementById('root'),
-// );
-
-// var app = new Vue({
-//   el: '#root',
-//   data: {
-//     message: 'Hello Vue!'
-//   }
-// })
-
 
 
 new Vue({
