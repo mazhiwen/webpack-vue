c4c3c99eabf54be4072b52dd2d829c620f9f67c6
81245995@qq.com <81245995@qq.com>
2019-11-30 15:08:04 +0800

    eharts

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,10 @@ const routes = [
     path: '/echartrelation',
     component: resolve => require(['views/echartrelation'], resolve),
   },
+  {
+    path: '/echartline',
+    component: resolve => require(['views/echartline'], resolve),
+  },
   // {
   //   path: '/home',
   //   component: Home,
