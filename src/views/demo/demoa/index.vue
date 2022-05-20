7d87948c29da8e20d7694a04aa850bdebe121083
mazhiwen <81245995@qq.com>
2022-05-20 18:06:50 +0800

    懒的写commit desc,shell push

--- src/views/demo/demoa/index.vue
+++ src/views/demo/demoa/index.vue
@@ -1,24 +1,6 @@
 <template>
   <div class="page_demoa">
 
-    <svg viewBox="-3 0 33 10" xmlns="http://www.w3.org/2000/svg">
-  <!-- No dash array -->
-  <line x1="0" y1="1" x2="30" y2="1" stroke="black" />
-
-  <!-- No dash offset -->
-  <line x1="0" y1="3" x2="30" y2="3" stroke="black"
-        stroke-dasharray="3 1" />
-
-        <line x1="0" y1="5" x2="30" y2="5" stroke="black"
-        stroke-dasharray="5"
-        stroke-dashoffset="2" />
-    </svg>
-    <div v-for="(item,index) in testlist"
-      class="blockitem"
-      :key="index"
-    >
-      {{item}}
-    </div>
   </div>
 </template>
 
@@ -42,6 +24,7 @@ export default class extends Vue{
   // props: {
 
   // },
+  thegif
   // newtestClass = newtestClass
   testlist = [1,2,3,4,5]
   show = false
--- src/views/demo/webgl/index.vue
+++ src/views/demo/webgl/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="map_wrap">
-    222
+    <div id="testbox" />
   </div>
 </template>
 
@@ -15,15 +15,13 @@ import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRe
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min';
 
 
+// import moon1024 from 'images/time.jpg';
 import earthatmos2048 from './earth_atmos_2048.jpg';
 import earthspecular2048 from './earth_specular_2048.jpg';
 
 import earthnormal2048 from './earth_normal_2048.jpg';
-// import earthatmos2048 from './earth_atmos_2048.jpg';
+import moon1024 from './moon_1024.jpg';
 
-// console.log(earthatmos2048);
-
-const testimg = 'https://github.com/mrdoob/three.js/blob/master/examples/textures/planets/earth_atmos_2048.jpg';
 
 export default {
   data() {
@@ -38,7 +36,7 @@ export default {
 
     let camera; let scene; let renderer; let
       labelRenderer;
-
+    let earth;
     const layers = {
 
       'Toggle Name': function () {
@@ -60,18 +58,7 @@ export default {
     const clock = new THREE.Clock();
     const textureLoader = new THREE.TextureLoader();
 
-    textureLoader.load(testimg, (texture) => {
-      // in this example we create the material when the texture is loaded
-      console.log(11, texture);
-    },
 
-    // onProgress callback currently not supported
-    undefined,
-
-    // onError callback
-    (err) => {
-      console.error('An error happened.', err);
-    });
     let moon;
 
 
@@ -85,16 +72,33 @@ export default {
       labelRenderer.setSize(window.innerWidth, window.innerHeight);
     }
 
-
+    let earthAnimateStatus = 'toNear';
     function animate() {
       requestAnimationFrame(animate);
 
-      const elapsed = clock.getElapsedTime();
+      // const elapsed = clock.getElapsedTime();
 
-      moon.position.set(Math.sin(elapsed) * 5, 0, Math.cos(elapsed) * 5);
+      // moon.position.set(Math.sin(elapsed) * 5, 0, Math.cos(elapsed) * 5);
+      // camera.position.set(0, 5.5, 15);
 
+
+      if (earth.position.z >= 2) {
+        earthAnimateStatus = 'toFar';
+      } else if (earth.position.z <= 0) {
+        earthAnimateStatus = 'toNear';
+      }
+      earth.rotation.y -= 0.01;
+      if (earthAnimateStatus === 'toNear') {
+        earth.position.y += 0.01;
+        earth.position.z += 0.01;
+      } else {
+        earth.position.y -= 0.01;
+        earth.position.z -= 0.01;
+      }
+
+      console.log('z', earth.position.z);
       renderer.render(scene, camera);
-      labelRenderer.render(scene, camera);
+      // labelRenderer.render(scene, camera);
     }
 
     //
@@ -109,122 +113,142 @@ export default {
     }
 
     function init() {
-      const EARTH_RADIUS = 1;
+      const EARTH_RADIUS = 12;
       const MOON_RADIUS = 0.27;
 
       camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 200);
-      camera.position.set(10, 5, 20);
+      // camera.position.set(10, 5, 20);
+      camera.position.set(0, 5.5, 15);
       camera.layers.enableAll();
       camera.layers.toggle(1);
 
       scene = new THREE.Scene();
 
-      const dirLight = new THREE.DirectionalLight(0xffffff);
-      dirLight.position.set(0, 0, 1);
-      dirLight.layers.enableAll();
-      scene.add(dirLight);
+      // 环境光
+      const ambient = new THREE.AmbientLight(0xffffff);
+      scene.add(ambient);
+      // 摄像光
+      // const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+      // dirLight.position.set(0, 0, 1);
+      // dirLight.layers.enableAll();
+      // scene.add(dirLight);
 
-      const axesHelper = new THREE.AxesHelper(5);
+      const axesHelper = new THREE.AxesHelper(10);
       axesHelper.layers.enableAll();
       scene.add(axesHelper);
 
       //
 
-      const earthGeometry = new THREE.SphereGeometry(EARTH_RADIUS, 16, 16);
+      const earthGeometry = new THREE.SphereGeometry(EARTH_RADIUS, 64, 16, 0, Math.PI * 2, 0.7, Math.PI);
       const earthMaterial = new THREE.MeshPhongMaterial({
         specular: 0x333333,
         shininess: 5,
-        map: textureLoader.load('https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fharmonyos.oss-cn-beijing.aliyuncs.com%2Fimages%2F202107%2F851342d26bceccf9c9c7820764a501f3cd6a29.png%3Fx-oss-process%3Dimage%2Fresize%2Cw_1158%2Ch_261&refer=http%3A%2F%2Fharmonyos.oss-cn-beijing.aliyuncs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654939482&t=74bcf34c209f74784cddb225810e58db'),
-        // specularMap: textureLoader.load('./earth_specular_2048.jpg'),
-        // normalMap: textureLoader.load('./earth_normal_2048.jpg'),
-        specularMap: textureLoader.load(testimg),
-        normalMap: textureLoader.load(testimg),
+        map: textureLoader.load(earthatmos2048),
+        specularMap: textureLoader.load(earthspecular2048),
+        normalMap: textureLoader.load(earthnormal2048),
         normalScale: new THREE.Vector2(0.85, 0.85),
       });
-      const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+      earth = new THREE.Mesh(earthGeometry, earthMaterial);
+      earth.position.set(0, -8.7, 0);
+      // earth.rotateX(0.5);
       scene.add(earth);
 
-      const moonGeometry = new THREE.SphereGeometry(MOON_RADIUS, 16, 16);
-      const moonMaterial = new THREE.MeshPhongMaterial({
-        shininess: 5,
-        // map: textureLoader.load('./moon_1024.jpg'),
-        map: textureLoader.load(testimg),
-      });
-      moon = new THREE.Mesh(moonGeometry, moonMaterial);
-      scene.add(moon);
+      // const moonGeometry = new THREE.SphereGeometry(MOON_RADIUS, 16, 16);
+      // const moonMaterial = new THREE.MeshPhongMaterial({
+      //   shininess: 5,
+      //   map: textureLoader.load(moon1024),
+      // });
+      // moon = new THREE.Mesh(moonGeometry, moonMaterial);
+      // scene.add(moon);
+
+      // 盘子面
+
+      const geometry = new THREE.CircleGeometry(7.5, 32);
+      const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+      const circle = new THREE.Mesh(geometry, material);
+      circle.position.set(0, 0.5, 0);
+      circle.rotateX(-3.14 / 2);
+      scene.add(circle);
 
       //
 
       earth.layers.enableAll();
-      moon.layers.enableAll();
+      // moon.layers.enableAll();
 
-      const earthDiv = document.createElement('div');
-      earthDiv.className = 'label';
-      earthDiv.textContent = 'Earth';
-      earthDiv.style.marginTop = '-1em';
-      const earthLabel = new CSS2DObject(earthDiv);
-      earthLabel.position.set(0, EARTH_RADIUS, 0);
-      earth.add(earthLabel);
-      earthLabel.layers.set(0);
+      // const earthDiv = document.createElement('div');
+      // earthDiv.className = 'label';
+      // earthDiv.textContent = 'Earth';
+      // earthDiv.style.marginTop = '-1em';
+      // const earthLabel = new CSS2DObject(earthDiv);
+      // earthLabel.position.set(0, EARTH_RADIUS, 0);
+      // earth.add(earthLabel);
+      // earthLabel.layers.set(0);
 
-      const earthMassDiv = document.createElement('div');
-      earthMassDiv.className = 'label';
-      earthMassDiv.textContent = '5.97237e24 kg';
-      earthMassDiv.style.marginTop = '-1em';
-      const earthMassLabel = new CSS2DObject(earthMassDiv);
-      earthMassLabel.position.set(0, -2 * EARTH_RADIUS, 0);
-      earth.add(earthMassLabel);
-      earthMassLabel.layers.set(1);
+      // const earthMassDiv = document.createElement('div');
+      // earthMassDiv.className = 'label';
+      // earthMassDiv.textContent = '5.97237e24 kg';
+      // earthMassDiv.style.marginTop = '-1em';
+      // const earthMassLabel = new CSS2DObject(earthMassDiv);
+      // earthMassLabel.position.set(0, -2 * EARTH_RADIUS, 0);
+      // earth.add(earthMassLabel);
+      // earthMassLabel.layers.set(1);
 
-      const moonDiv = document.createElement('div');
-      moonDiv.className = 'label';
-      moonDiv.textContent = 'Moon';
-      moonDiv.style.marginTop = '-1em';
-      const moonLabel = new CSS2DObject(moonDiv);
-      moonLabel.position.set(0, MOON_RADIUS, 0);
-      moon.add(moonLabel);
-      moonLabel.layers.set(0);
+      // const moonDiv = document.createElement('div');
+      // moonDiv.className = 'label';
+      // moonDiv.textContent = 'Moon';
+      // moonDiv.style.marginTop = '-1em';
+      // const moonLabel = new CSS2DObject(moonDiv);
+      // moonLabel.position.set(0, MOON_RADIUS, 0);
+      // moon.add(moonLabel);
+      // moonLabel.layers.set(0);
 
-      const moonMassDiv = document.createElement('div');
-      moonMassDiv.className = 'label';
-      moonMassDiv.textContent = '7.342e22 kg';
-      moonMassDiv.style.marginTop = '-1em';
-      const moonMassLabel = new CSS2DObject(moonMassDiv);
-      moonMassLabel.position.set(0, -2 * MOON_RADIUS, 0);
-      moon.add(moonMassLabel);
-      moonMassLabel.layers.set(1);
+      // const moonMassDiv = document.createElement('div');
+      // moonMassDiv.className = 'label';
+      // moonMassDiv.textContent = '7.342e22 kg';
+      // moonMassDiv.style.marginTop = '-1em';
+      // const moonMassLabel = new CSS2DObject(moonMassDiv);
+      // moonMassLabel.position.set(0, -2 * MOON_RADIUS, 0);
+      // moon.add(moonMassLabel);
+      // moonMassLabel.layers.set(1);
 
       //
+      const width = 1100;
+      const height = 700;
 
       renderer = new THREE.WebGLRenderer();
       renderer.setPixelRatio(window.devicePixelRatio);
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      document.body.appendChild(renderer.domElement);
+      renderer.setSize(width, height);
+      renderer.setClearColor(0xb9d3ff, 1);
+      document.getElementById('testbox').appendChild(renderer.domElement);
 
       labelRenderer = new CSS2DRenderer();
-      labelRenderer.setSize(window.innerWidth, window.innerHeight);
+      labelRenderer.setSize(width, height);
       labelRenderer.domElement.style.position = 'absolute';
       labelRenderer.domElement.style.top = '0px';
-      document.body.appendChild(labelRenderer.domElement);
+      document.getElementById('testbox').appendChild(labelRenderer.domElement);
 
       const controls = new OrbitControls(camera, labelRenderer.domElement);
       controls.minDistance = 5;
       controls.maxDistance = 100;
 
-      //
+      // renderer.render(scene, camera);
+      // window.addEventListener('resize', onWindowResize);
 
-      window.addEventListener('resize', onWindowResize);
-
-      initGui();
+      // initGui();
     }
     init();
-    animate();
+    // animate();
   },
 };
 </script>
 
 <style lang="less">
 .map_wrap{
-
+  .label {
+    color: #FFF;
+    font-family: sans-serif;
+    padding: 2px;
+    background: rgba( 0, 0, 0, .6 );
+  }
 }
 </style>
